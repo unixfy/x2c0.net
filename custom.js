865d277a3baddc3ed95514cb3ddd06157c112e9d
Alex Wang <admin@x2c0.net>
2018-06-29 18:34:45 -0400

    Typed.js (and some CSS)
    Added typed.js strings and fixed up some CSS regarding the alignment and positioning of the first div

--- custom.js
+++ custom.js
@@ -1,3 +1,19 @@
+// sidenav
 $(document).ready(function(){
   $('.sidenav').sidenav();
+});
+
+// TypedJS
+$(window).on('load', function() {
+  var typed = new Typed("#hello", {
+    stringsElement: '#hellos',
+    typeSpeed: 75,
+    startDelay: 1000,
+    backSpeed: 75,
+    backDelay: 2000,
+    loop: true,
+    loopCount: Infinity,
+    autoInsertCss: false,
+    smartBackspace: false,
+  });
 });
